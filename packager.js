886ec354ec035f8dcea0e84e2b0b64667764bd43
1vasari <onevasari@gmail.com>
2015-12-17 10:45:53 +1000

    Use new songdown-packager to handle packaging songs.

--- packager.js
+++ packager.js
@@ -1,56 +1,11 @@
-'use strict';
+'use strict'
 
-var _ = require('lodash');
-var glob = require('glob');
-var normalizeNewline = require('normalize-newline');
-var gray = require('gray-matter');
+let packager = require('songdown-packager')
+let path = require('path')
 
-var path = require('path');
-var fs = require('fs');
+const source = path.join(__dirname, 'songs')
+const destination = __dirname
 
-var SONGS_DIR = path.join(__dirname, 'songs');
-
-var GRAY_MATTER_CONFIG = {
-  delims: ['===', '===']
-};
-
-var run = function() {
-  var options = {
-    cwd: SONGS_DIR
-  };
-
-  var files = glob.sync('*.songdown', options);
-  var output = {};
-
-  _.each(files, function(file) {
-    var splitName = file.split(' - ');
-    var artist = splitName.shift();
-    var name = splitName.join(' ').replace(/\.songdown$/, '');
-
-    var location = path.join(SONGS_DIR, file);
-    var source = normalizeNewline(fs.readFileSync(location).toString());
-
-    var matter = gray(source, GRAY_MATTER_CONFIG);
-
-    // Remove front matter from the source so it doesn't appear to the user.
-    source = matter.content;
-
-    var obj = {
-      artist: artist,
-      name: name,
-      source: source
-    };
-
-    _.merge(obj, matter.data);
-
-    if (!output[artist]) {
-      output[artist] = {};
-    }
-
-    output[artist][name] = obj;
-  });
-
-  fs.writeFileSync(path.join(__dirname, 'songs.json'), JSON.stringify(output, null, 2));
-};
-
-run();
+console.log('Packaging Songdown songs')
+packager.run(source, destination)
+console.log('Done')
